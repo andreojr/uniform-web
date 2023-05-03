d48c8d42047aa6017092125fecb90fedb66c0029
andreojr <atipicode.ec@gmail.com>
2023-05-03 16:54:22 -0300

    up

--- src/pages/Pay.tsx
+++ src/pages/Pay.tsx
@@ -8,18 +8,23 @@ import { Check, CircleNotch, Copy, Info, WhatsappLogo } from "@phosphor-icons/re
 import copy from "copy-to-clipboard";
 import clsx from "clsx";
 
-interface PixInfoProps {
+import qrcode1 from "../assets/qrcode_1x.jpeg";
+import qrcode2 from "../assets/qrcode_2x.jpeg";
+import qrcode3 from "../assets/qrcode_3x.jpeg";
+import qrcode4 from "../assets/qrcode_4x.jpeg";
+
+export const freteTotal = 52.2;
+
+interface Payment {
     hash: string;
     qrcode: string;
 }
-
-export const freteTotal = 52.2;
 
 export function Pay() {
     const { user_id } = useParams();
     const [frete, setFrete] = useState<number | null>(null);
     const [value, setValue] = useState<number | null>(null);
-    const [pixInfo, setPixInfo] = useState<PixInfoProps | null>(null);
+    const [pay, setPay] = useState<Payment | null>(null);
     const [copied, setCopied] = useState(false);
 
     useEffect(() => {
@@ -28,17 +33,38 @@ export function Pay() {
                 console.log(response.data);
                 setValue(response.data.count * precoUnitario);
                 setFrete(response.data.frete);
-                setPixInfo(response.data.pixInfo);
             }
         });
     }, []);
+
+    useEffect(() => {
+        if (value) {
+            const count = value / precoUnitario;
+            const payUpdate = {} as Payment;
+            if (count === 1) {
+                payUpdate.qrcode = qrcode1;
+                payUpdate.hash = "00020101021126720014br.gov.bcb.pix0136f0bdb4f2-57d8-4db8-bf88-971a72076ffb02101x UniForm520400005303986540527.675802BR5919ANDRE L DE O JUNIOR6008SALVADOR62070503***6304DC59";
+            } else if (count === 2) {
+                payUpdate.qrcode = qrcode2;
+                payUpdate.hash = "00020101021126720014br.gov.bcb.pix0136f0bdb4f2-57d8-4db8-bf88-971a72076ffb02102x UniForm520400005303986540555.345802BR5919ANDRE L DE O JUNIOR6008SALVADOR62070503***63047208";
+            } else if (count === 3) {
+                payUpdate.qrcode = qrcode3;
+                payUpdate.hash = "00020101021126720014br.gov.bcb.pix0136f0bdb4f2-57d8-4db8-bf88-971a72076ffb02103x UniForm520400005303986540583.015802BR5919ANDRE L DE O JUNIOR6008SALVADOR62070503***63048726";
+            } else if (count === 4) {
+                payUpdate.qrcode = qrcode4;
+                payUpdate.hash = "00020101021126720014br.gov.bcb.pix0136f0bdb4f2-57d8-4db8-bf88-971a72076ffb02104x UniForm5204000053039865406110.685802BR5919ANDRE L DE O JUNIOR6008SALVADOR62070503***63043D36";
+            }
+
+            setPay(payUpdate);
+        }
+    }, [value]);
 
     function handleCopyHash(str: string) {
         copy(str);
         setCopied(true);
     }
 
-    return (frete && value && pixInfo) ? (
+    return (frete && value && pay) ? (
         <div className="text-white flex flex-col py-12">
             <Link className="flex flex-col" to="/">
                 <img src={logo} alt="UniForm" className="h-7" />
@@ -47,17 +73,17 @@ export function Pay() {
                 <div className="overflow-hidden rounded-md">
                     <div
                         style={{
-                            backgroundImage: `url(${pixInfo.qrcode})`,
+                            backgroundImage: `url(${pay.qrcode})`,
                             backgroundPosition: 'center',
                             backgroundSize: 'cover',
                             backgroundRepeat: 'no-repeat',
                         }}
-                        className="w-[14rem] h-[14rem] rounded-md"
+                        className="w-[14rem] h-[14rem] rounded-md scale-[1.8]"
                     />
                 </div>
                 <div className="bg-zinc-700 w-[14rem] p-4 rounded-md relative">
-                    <p className="text-xs w-full break-all text-zinc-400">{pixInfo.hash}</p>
-                    <button onClick={() => handleCopyHash(pixInfo.hash)} className={clsx("text-white flex items-center justify-center gap-3 w-full absolute bottom-0 left-0 rounded-md py-3", {
+                    <p className="text-xs w-full break-all text-zinc-400">{pay.hash}</p>
+                    <button onClick={() => handleCopyHash(pay.hash)} className={clsx("text-white flex items-center justify-center gap-3 w-full absolute bottom-0 left-0 rounded-md py-3", {
                         "bg-violet-600": !copied,
                         "bg-green-500": copied,
                     })}>
